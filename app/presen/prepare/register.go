2e03da47dee2582cf647daafbb6db4710dc5f2c8
Cassin01 <koyojb@gmail.com>
2024-11-12 00:55:19 +0900

    update: register.go, queryservice,

--- app/presen/prepare/register.go
+++ app/presen/prepare/register.go
@@ -1,6 +1,10 @@
 package prepare
 
 import (
+	"context"
+
+	"queryservice/presen/interceptor"
+
 	"github.com/Cassin01/samplepb/pb"
 	"google.golang.org/grpc"
 )
@@ -10,10 +14,28 @@ type QueryServer struct {
 	Server *grpc.Server // gRPCServer
 }
 
+// インターセプタをチェーン化して実行する
+func chainUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	// LoggingInterceptor内部でUUIDValidationInterceptorと
+	// handlerを呼び出す新しいhandlerを作成する
+	newHandler := func(currentCtx context.Context, currentReq interface{}) (interface{}, error) {
+		// UUID形式の検証インターセプタを実行する
+		return interceptor.UUIDValidationInterceptor(currentCtx, currentReq, info, handler)
+	}
+	// ログ出力インターセプタを実行する
+	return interceptor.LoggingInterceptor(ctx, req, info, newHandler)
+}
+
 // コンストラクタ
 func NewQueryServer(category pb.CategoryQueryServer, product pb.ProductQueryServer) *QueryServer {
+	// インターセプタの追加
+	serverOpts := []grpc.ServerOption{
+		// ログ出力、入力検証インターセプタを登録インターセプタを登録
+		grpc.UnaryInterceptor(chainUnaryInterceptor),
+	}
+
 	// gRPCサーバを生成する(インターセプタの追加)
-	server := grpc.NewServer()
+	server := grpc.NewServer(serverOpts...)
 
 	// CategoryQueryServerを登録する
 	pb.RegisterCategoryQueryServer(server, category)
